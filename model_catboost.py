ee26de7f9846bb51468fe4eb67647381f261c749
Clement Pottier <clement.pottier@polytechnique.edu>
2024-12-10 10:57:20 +0100

    Change of rush hour

--- model_catboost.py
+++ model_catboost.py
@@ -32,8 +32,8 @@ def prepare_data(df, is_train=None):
 
     # Rush hour
     df['is_working_day'] = (~df['is_weekend']) & (~df['is_holiday'])
-    morning_rush = (df['hour'].between(7,9)) & df['is_working_day']
-    evening_rush = (df['hour'].between(16,18)) & df['is_working_day']
+    morning_rush = (df['hour'].between(6,8)) & df['is_working_day']
+    evening_rush = (df['hour'].between(15,17)) & df['is_working_day']
     df['is_rush_hour'] = (morning_rush | evening_rush)
 
     # Drop unused columns
